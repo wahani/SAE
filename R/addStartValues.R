d084abeb036bfe3a2ce4a4c7b6f02e41a3511aab
Sebastian <wahani@gmail.com>
2014-06-13 13:44:58 +0200

    bugfix in if

--- R/addStartValues.R
+++ R/addStartValues.R
@@ -11,7 +11,7 @@ addStartValues.default <- function(modelSpecs) {
 addStartValues.MSRFH <- function(modelSpecs) {
   # Generic function: computes starting values
   resultFH <- eval(parse(text = paste("sae::eblupFH(modelSpecs$formula,", modelSpecs$vardirName, ",data=modelSpecs$data)")))
-  if(is.na(resultFH$fit$estcoef)) stop("Starting values are NAs!")
+  if(any(is.na(resultFH$fit$estcoef))) stop("Starting values are NAs!")
   modelSpecs$beta <- resultFH$fit$estcoef$beta
   modelSpecs$reVar <- resultFH$fit$refvar
   modelSpecs
